8cdc892de8f2282fcdbddbd08dd0006cbe29d911
jinjunho <junho11_kr@naver.com>
2024-11-29 19:27:51 +0900

    up

--- src/DropAndDrop/DropTest.jsx
+++ src/DropAndDrop/DropTest.jsx
@@ -39,41 +39,64 @@ const DragItems = styled.div`
     border: 2px solid gray;
     transform: translateX(50px);
     margin-bottom: 2px;
-
+    font-size: 35px;
+    text-align: center;
 `;
 
 function DropTest() {
-    const [dragUI, setDragUI] = useState(['item1', 'item2', 'item3', 'item4']);
+    const [dragUI, setDragUI] = useState([
+        { id: 1, value: 'item1' },
+        { id: 2, value: 'item2' },
+        { id: 3, value: 'item3' },
+        { id: 4, value: 'item4' }
+    ]);
+
     const [dragBin, setDragBin] = useState([]);
 
-    const handDrag = (data, item) => {
-        data.dataTransfer.setData('text/plain',item);
+    const handDrag = (data, id) => {
+        data.dataTransfer.setData('text/plain', id);
     };
 
     // const handDrag_1 = (data) => {
-    //     const items = data.dataTransfer.getData('text/plain');
-    //     setDragBin((event) => event.filter((i) =>(i !== items)));
-    //     setDragUI((event) => ([...event, items]));
+    //     const items = data.dataTransfer.getData('text/plain'); // 아이템 가져오기
+    //     setDragBin((event) => event.filter((i) =>(i !== items))); // 빈배열 삭제
+    //     setDragUI((event) => ([...event, items])); // UI 아이템 추가
     // };
 
-    const handDrag_1 = (data, item) => {
-        data.dataTransfer.getData('text/plain', item);
-        setDragBin((event) => event.filter((i) =>(i !== item)));
-        setDragUI((event) => ([...event, item]));
-    };
-
     // const handDrag_2 = (data) => {
-    //     const items = data.dataTransfer.getData('text/plain');
+    //     const items = data.dataTransfer.getData('text/plain'); // 아이템 가져오기
+    //     setDragUI((event) => event.filter((i) =>(i !== items))); // UI 삭제
+    //     setDragBin((event) => ([...event, items])); // 빈베열에 추가
+    // };
+
+    const handDrag_1 = (data) => {
+        // parseInt 문자열을 정수로 바꿔주는 명령어
+        // find 주어진 조건에 만족하는 값은 찾는 명령어
+        const itemId = parseInt(data.dataTransfer.getData('text/plain'), 10); // 10은 10진수 변환해주려고
+        const movedItem = dragBin.find((item) => item.id === itemId);
+    
+        if (movedItem) {
+            setDragBin((prev) => prev.filter((item) => item.id !== itemId));
+            setDragUI((prev) => [...prev, movedItem]);
+        }
+    };
+    
+    const handDrag_2 = (data) => {
+        const itemId = parseInt(data.dataTransfer.getData('text/plain'), 10);
+        const movedItem = dragUI.find((item) => item.id === itemId);
+    
+        if (movedItem) {
+            setDragUI((prev) => prev.filter((item) => item.id !== itemId));
+            setDragBin((prev) => [...prev, movedItem]);
+        }
+    };    
+
+    // 이 코드가 안되는 이유 getData('text/plain', items) items라는 두번째 매개변수를 받아올 수가 없기 때문이다.
+    // const handDrag_2 = (data, items) => {
+    //     data.dataTransfer.getData('text/plain', items);
     //     setDragUI((event) => event.filter((i) =>(i !== items)));
     //     setDragBin((event) => ([...event, items]));
     // };
-
-    // 이 코드가 안되는 이유 getData('text/plain', items) items라는 두번째 매개변수를 받아올 수가 없기 때문이다.
-    const handDrag_2 = (data, items) => {
-        data.dataTransfer.getData('text/plain', items);
-        setDragUI((event) => event.filter((i) =>(i !== items)));
-        setDragBin((event) => ([...event, items]));
-    };
 
     const handleDragOver = (event) => {
         event.preventDefault();
@@ -86,13 +109,13 @@ function DropTest() {
                 onDragOver={handleDragOver}
             >
                 <span className='d_1'>드래그1</span>
-                {dragUI.map((index) => (
+                {dragUI.map((item) => (
                     <DragItems
-                        key={index}
+                        key={item.id}
                         draggable
-                        onDragStart={(data) => handDrag(data, index)}
+                        onDragStart={(data) => handDrag(data, item.id)}
                     >
-                        {index}
+                        {item.value}
                     </DragItems>
                 ))}
             </Drag_1>
@@ -102,14 +125,13 @@ function DropTest() {
                 onDragOver={handleDragOver}
             >
                 <span className='d_2'>드래그2</span>
-
-                {dragBin.map((items,index) => (
+                {dragBin.map((item) => (
                     <DragItems
-                        key={index}
+                        key={item.id}
                         draggable
-                        onDragStart={(data) => handDrag(data, index)}
+                        onDragStart={(data) => handDrag(data, item.id)}
                     >
-                        {items}
+                        {item.value}
                     </DragItems>
                 ))}
             </Drag_2>
